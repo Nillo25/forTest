b03425dfb67a0f9f956f2d7f08a4aa4017f774da
Nillo25 <chechnev.p@icloud.com>
2020-06-16 14:05:36 +0300

    Итоги урока 1

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -7,24 +7,18 @@ public class MyFirstProgram {
     hello("Pavel");
     hello("user");
 
-    double i = 5;
-    double a = 3;
-    double b = 6;
 
-      System.out.println("Площадь квадрата со стороной " + i + " = " + area(i));
-      System.out.println("Площадь прямоугольника со стронами " + a + " и " + b + " = " + area(a, b));
+    Square s = new Square(5);
+    System.out.println("Площадь квадрата со стороной " + s.i + " = " + s.area());
+
+    Rectangle r = new Rectangle(4, 6);
+    System.out.println("Площадь прямоугольника со стронами " + r.a + " и " + r.b + " = " + r.area());
   }
 
 
-  public static void  hello(String somebody){
-      System.out.println("Hello, " + somebody + "!");
+  public static void hello(String somebody) {
+    System.out.println("Hello, " + somebody + "!");
   }
 
 
-  public static double area(double len){
-      return len * len;
-  }
-  public  static  double area(double a, double b){
-      return a * b;
-  }
 }
