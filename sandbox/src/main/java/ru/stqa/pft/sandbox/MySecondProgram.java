0a1c90729d8dade1ca5cf28094309558d547ba1f
Nillo25 <chechnev.p@icloud.com>
2020-06-18 17:41:16 +0300

    Добавил сканер, думаю так лучше будет.

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MySecondProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MySecondProgram.java
@@ -1,5 +1,7 @@
 package ru.stqa.pft.sandbox;
 
+import java.util.Scanner;
+
 import static ru.stqa.pft.sandbox.Point.distance;
 
 public class MySecondProgram {
@@ -7,8 +9,18 @@ public class MySecondProgram {
 
 
   public static void main(String[] args) {
-    Point p1 = new Point(0.0, 0.0);
-    Point p2 = new Point(1.0, 0.0);
+    Scanner in = new Scanner(System.in);
+    System.out.println("введите координаты X первой точки");
+    double numX1 = in.nextDouble();
+    System.out.println("введите координаты Y первой точки");
+    double numY1 = in.nextDouble();
+    System.out.println("введите координаты X второй точки");
+    double numX2 = in.nextDouble();
+    System.out.println("введите координаты Y второй точки");
+    double numY2 = in.nextDouble();
+    Point p1 = new Point(numX1,numY1);
+    Point p2 = new Point(numX2,numY2);
+    in.close();
 
 
     System.out.println("Расстояние между точками (" + p1.point1() + ") и (" + p2.point2() + ") = " + distance(p1, p2));
--- sandbox/src/test/java/ru/stqa/pft/sandbox/PointTest.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTest.java
@@ -10,11 +10,20 @@ public class PointTest {
 
   @Test
 
-  public void testPoint() {
+  public void testPoint1() {
 
     Point p1 = new Point(0, 0);
     Point p2 = new Point(1, 0);
     Assert.assertEquals(distance(p1, p2), 1);
+  }
+
+  @Test
+
+  public void testPoint2() {
+
+    Point p1 = new Point(0, 0);
+    Point p2 = new Point(10, 10);
+    Assert.assertEquals(distance(p1, p2), 14.142135623730951);
   }
 
 
